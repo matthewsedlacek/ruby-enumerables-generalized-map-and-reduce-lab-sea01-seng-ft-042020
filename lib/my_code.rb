8c612a8146f288ebb31693710f4751081eb140a7
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-29 00:43:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,10 @@ def map (source_array)
 end
 
 def reduce (source_array, starting_point = nil)
-  
+  if starting_point
+    sum = starting_point
+  else
+    
   
   i = 0
   total = starting_point
